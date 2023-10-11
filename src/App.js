a4d23856845529e03d00c22fe2860c5eae9e02f7
Godslove Udo <80900180+G0d5l0v3@users.noreply.github.com>
2023-10-11 14:49:23 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,26 +2,36 @@ import React, { useState } from "react";
 
 function App() {
   const message = ["Learn React", "Apply for a job", "Invest your new income"];
-  const [ step, setStep ] = useState(1)
-  const stepAddRange = step <=2
-  const stepMinusRange = step >=2
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true)
+  const stepAddRange = step < 3;
+  const stepMinusRange = step > 1;
 
   function addStep() {
-    if(stepAddRange){
-      setStep( step + 1)
+    if (stepAddRange) {
+      setStep((step) => step + 1);
     }
   }
 
   function minusStep() {
-    if(stepMinusRange){
-      setStep( step - 1)
+    if (stepMinusRange) {
+      setStep((s) => s - 1);
     }
   }
 
+  function close() {
+    setIsOpen((open) => !open)
+  }
+  
+
   return (
-    <React.Fragment className="App">
+    <React.Fragment>
+      <div className="flex items-center justify-end px-[3rem] pt-3">
+        <button className="font-[inter] text-sm" onClick = {close}> &times; </button>
+      </div>
+
       <div className="flex items-center justify-center min-h-screen font-[inter]">
-        <div className="w-[20rem] bg-[#F6F6F6] h-[auto] px-[3rem] py-[1rem] space-y-6">
+       { isOpen && (<div className= "w-[20rem] bg-[#F6F6F6] h-[auto] px-[3rem] py-[1rem] space-y-6" >
           <div className="flex justify-between items-center ">
             <span
               className={`rounded-full h-8 w-8 flex items-center justify-center ${
@@ -46,14 +56,23 @@ function App() {
             </span>
           </div>
           <div className="text-center font-bold">
-            
             Step {step}: {message[step - 1]}
           </div>
           <div className="flex justify-between items-center  ">
-            <button onClick = { minusStep } className="bg-[#6D45F1] px-3 py-1 text-sm text-white rounded-full">Previous</button>
-            <button onClick = { addStep } className="bg-[#6D45F1] px-3 py-1 text-sm text-white rounded-full">Next</button>
+            <button
+              onClick={minusStep}
+              className="bg-[#6D45F1] px-3 py-1 text-sm text-white rounded-full"
+            >
+              Previous
+            </button>
+            <button
+              onClick={addStep}
+              className="bg-[#6D45F1] px-3 py-1 text-sm text-white rounded-full"
+            >
+              Next
+            </button>
           </div>
-        </div>
+        </div>) }
       </div>
     </React.Fragment>
   );
